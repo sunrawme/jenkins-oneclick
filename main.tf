e36f3c280370da683420650663feb3e39ec3e3e7
sunrawme <71823093+sunrawme@users.noreply.github.com>
2026-06-29 17:21:06 +0530

    Update AMI ID for aws_instance resource

--- main.tf
+++ main.tf
@@ -188,7 +188,7 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "sonar_nodes" {
   count                  = 2
-  ami                    = data.aws_ami.ubuntu.id
+  ami                    = "ami-0f8a61b66d1accaee"
   instance_type          = var.instance_type
   subnet_id              = aws_subnet.private[count.index].id
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
