024f82a0511f310b6878399e036ad0fa298ebcbb
sunrawme <71823093+sunrawme@users.noreply.github.com>
2026-06-30 15:56:53 +0530

    Add sonar_instance_ids output to outputs.tf
    Add output for EC2 Instance IDs required by AWS SSM.

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,11 @@ output "sonar_node_private_ips" {
   value = aws_instance.sonar_nodes[*].private_ip
 }
 
+output "sonar_instance_ids" {
+  description = "The EC2 Instance IDs required by AWS SSM Session Manager"
+  value       = aws_instance.sonar_nodes[*].id
+}
+
 output "efs_dns_name" {
   value = aws_efs_file_system.sonar_shared.dns_name
 }
