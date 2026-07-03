fef07a40f32da5a73e0b5db1398e4b9b7d6dba0b
sunrawme <71823093+sunrawme@users.noreply.github.com>
2026-07-03 15:51:18 +0530

    Remove bastion_private_ip output from network_and_bastion.tf
    Remove output for bastion_private_ip from Terraform configuration.

--- network_and_bastion.tf
+++ network_and_bastion.tf
@@ -103,7 +103,4 @@ output "bastion_public_ip" {
   value       = aws_instance.bastion.public_ip
 }
 
-output "bastion_private_ip" {
-  description = "The private IP address of the bastion host"
-  value       = aws_instance.bastion.private_ip
-}
+
