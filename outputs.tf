afe3d62b724fa853042e0cb5f8a06e9fc093e209
sunrawme <71823093+sunrawme@users.noreply.github.com>
2026-07-03 15:50:24 +0530

    Add bastion public IP output
    Added output for the public IP address of the bastion host.

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,9 @@
 output "bastion_private_ip" {
-  description = "The private IP address of the Bastion host"
+  description = "The private IP address of the bastion host"
   value       = aws_instance.bastion.private_ip
+}
+
+output "bastion_public_ip" {
+  description = "The public IP address of the bastion host"
+  value       = aws_instance.bastion.public_ip
 }
