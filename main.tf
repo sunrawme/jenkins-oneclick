d862f3356ac9ec9b0ac01860a60a6928480dce1e
sunrawme <71823093+sunrawme@users.noreply.github.com>
2026-07-03 12:34:19 +0530

    Delete EC2 SSM IAM roles and policies
    Removed IAM roles and policy attachments for EC2 SSM.

--- main.tf
+++ main.tf
@@ -316,31 +316,3 @@ resource "aws_cloudwatch_metric_alarm" "tg2_unhealthy" {
   }
 }
 
-# ==============================================================================
-# --- IAM ROLES & POLICY ATTACHMENTS ---
-# ==============================================================================
-
-resource "aws_iam_role" "ec2_ssm_role" {
-  name = "sonarqube-ec2-ssm-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action    = "sts:AssumeRole"
-        Effect    = "Allow"
-        Principal = { Service = "ec2.amazonaws.com" }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "ssm_policy" {
-  role       = aws_iam_role.ec2_ssm_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-}
-
-resource "aws_iam_instance_profile" "ec2_ssm_profile" {
-  name = "sonarqube-ec2-ssm-profile"
-  role = aws_iam_role.ec2_ssm_role.name
-}
