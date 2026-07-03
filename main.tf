7f9ecf2f9dd0b60d073229989ffde4efac794f5f
sunrawme <71823093+sunrawme@users.noreply.github.com>
2026-07-03 15:00:24 +0530

    Implement monitoring and alerts for SonarQube
    Added SNS topic and CloudWatch alarms for monitoring.

--- main.tf
+++ main.tf
@@ -156,3 +156,68 @@ resource "aws_autoscaling_group" "sonar_asg_az2" {
     propagate_at_launch = true
   }
 }
+
+# ==============================================================================
+# --- MONITORING & ALERTS ---
+# ==============================================================================
+
+resource "aws_sns_topic" "alerts" {
+  name = "sonarqube-alerts-topic"
+}
+
+resource "aws_sns_topic_subscription" "email_target" {
+  topic_arn = aws_sns_topic.alerts.arn
+  protocol  = "email"
+  endpoint  = "sunraw541@gmail.com"
+}
+
+resource "aws_cloudwatch_metric_alarm" "asg_az1_cpu" {
+  alarm_name          = "sonarqube-asg-az1-high-cpu"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "120"
+  statistic           = "Average"
+  threshold           = "80"
+  alarm_description   = "Monitors average CPU load across ASG Pinned to AZ1"
+  alarm_actions       = [aws_sns_topic.alerts.arn]
+
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.sonar_asg_az1.name
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "asg_az2_cpu" {
+  alarm_name          = "sonarqube-asg-az2-high-cpu"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "120"
+  statistic           = "Average"
+  threshold           = "80"
+  alarm_description   = "Monitors average CPU load across ASG Pinned to AZ2"
+  alarm_actions       = [aws_sns_topic.alerts.arn]
+
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.sonar_asg_az2.name
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "tg2_unhealthy" {
+  alarm_name          = "sonarqube-tg-az2-unhealthy-alarm"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "UnHealthyHostCount"
+  namespace           = "AWS/ApplicationELB"
+  period              = "60"
+  statistic           = "Average"
+  threshold           = "1"
+  alarm_actions       = [aws_sns_topic.alerts.arn]
+
+  dimensions = {
+    TargetGroup  = aws_lb_target_group.sonar_tg_az2.arn_suffix
+    LoadBalancer = aws_lb.sonar_alb.arn_suffix
+  }
+}
