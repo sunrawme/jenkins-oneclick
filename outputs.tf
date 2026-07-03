b78ae6b6eca412ddfb5f39c26980c6830beff36a
sunrawme <71823093+sunrawme@users.noreply.github.com>
2026-07-03 14:54:48 +0530

    Refactor outputs.tf to simplify output descriptions
    Removed unnecessary details from bastion_private_ip output description and removed sonar_node_ips output.

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,4 @@
 output "bastion_private_ip" {
-  description = "The private IP address of the Bastion host jump-box"
+  description = "The private IP address of the Bastion host"
   value       = aws_instance.bastion.private_ip
-}
-
-output "sonar_node_ips" {
-  description = "Note: Nodes are managed by ASG, their private IPs can be fetched via AWS CLI if needed"
-  value       = "Dynamic via ASG"
 }
