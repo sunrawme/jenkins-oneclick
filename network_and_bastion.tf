bbd69d9c0f8ea3e10a87738154e9a8b07664f71b
sunrawme <71823093+sunrawme@users.noreply.github.com>
2026-07-06 16:38:32 +0530

    Update key name for bastion EC2 instance

--- network_and_bastion.tf
+++ network_and_bastion.tf
@@ -115,7 +115,7 @@ resource "aws_security_group" "bastion_sg" {
 resource "aws_instance" "bastion" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
-  key_name      = "sandeep-key"
+  key_name      = "sonarkey"
   subnet_id     = aws_subnet.public[0].id
   vpc_security_group_ids = [aws_security_group.bastion_sg.id]
 
