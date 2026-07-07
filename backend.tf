ee3b807926ff37d11257f0ecc332955bdc36e70e
sunrawme <71823093+sunrawme@users.noreply.github.com>
2026-07-07 12:26:59 +0530

    Configure Terraform backend with S3 settings

--- backend.tf
+++ backend.tf
@@ -1,1 +1,8 @@
-
+terraform {
+  backend "s3" {
+    bucket         = "sandeep0010demo"
+    key            = "sonarqube/infrastructure/terraform.tfstate"
+    region         = "ap-south-1" # Matches your infrastructure region
+    encrypt        = true
+  }
+}
