ed391c374beb9d432e6600a49d3c9db538a71bbb
sunrawme <71823093+sunrawme@users.noreply.github.com>
2026-07-09 10:18:17 +0530

    Delete bastion public IP output block
    Removed output block for bastion public IP.

--- network_and_bastion.tf
+++ network_and_bastion.tf
@@ -109,7 +109,4 @@ resource "aws_instance" "bastion" {
   tags = { Name = "bastion-host" }
 }
 
-# --- IMPORTANT: EXPORT BASTION IP ---
-output "bastion_public_ip" {
-  value = aws_instance.bastion.public_ip
-}
+
