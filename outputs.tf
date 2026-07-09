648cf391a5a687e3395c797415aa7f4357cca134
sunrawme <71823093+sunrawme@users.noreply.github.com>
2026-07-09 10:25:31 +0530

    Remove sonar instance private IP outputs
    Removed outputs for sonar_active_private_ip and sonar_passive_private_ip.

--- outputs.tf
+++ outputs.tf
@@ -7,11 +7,4 @@ output "bastion_public_ip" {
   value = aws_instance.bastion.public_ip # Make sure the resource name matches yours
 }
 
-output "sonar_active_private_ip" {
-  # This retrieves the private IP of the instance created by your ASG
-  value = element(flatten(aws_autoscaling_group.sonar_asg_az1.*.instances), 0)
-}
 
-output "sonar_passive_private_ip" {
-  value = element(flatten(aws_autoscaling_group.sonar_asg_az2.*.instances), 0)
-}
